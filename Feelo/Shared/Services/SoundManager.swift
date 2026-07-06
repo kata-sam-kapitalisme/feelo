804b32728411758fac2e7c0cb19f9a8cf2c8e33d
rfart <firdausamerta@gmail.com>
2026-07-06 20:50:17 +0700

    Enter level sound effect

--- Feelo/Shared/Services/SoundManager.swift
+++ Feelo/Shared/Services/SoundManager.swift
@@ -2,20 +2,29 @@ import AVFoundation
 
 final class SoundManager {
     static let shared = SoundManager()
-    private var player: AVAudioPlayer?
+    private var clickPlayer: AVAudioPlayer?
+    private var levelUpPlayer: AVAudioPlayer?
 
     private init() {
-        guard let url = Bundle.main.url(
-            forResource: "floraphonic-casual-click-pop-ui-4-262121",
-            withExtension: "mp3"
-        ) else { return }
-        player = try? AVAudioPlayer(contentsOf: url)
-        player?.prepareToPlay()
+        if let url = Bundle.main.url(forResource: "floraphonic-casual-click-pop-ui-4-262121", withExtension: "mp3") {
+            clickPlayer = try? AVAudioPlayer(contentsOf: url)
+            clickPlayer?.prepareToPlay()
+        }
+        if let url = Bundle.main.url(forResource: "universfield-level-up-02-199574", withExtension: "mp3") {
+            levelUpPlayer = try? AVAudioPlayer(contentsOf: url)
+            levelUpPlayer?.prepareToPlay()
+        }
     }
 
     func playClick() {
-        player?.stop()
-        player?.currentTime = 0
-        player?.play()
+        clickPlayer?.stop()
+        clickPlayer?.currentTime = 0
+        clickPlayer?.play()
+    }
+
+    func playLevelUp() {
+        levelUpPlayer?.stop()
+        levelUpPlayer?.currentTime = 0
+        levelUpPlayer?.play()
     }
 }
